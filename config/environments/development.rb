7d851ff87eebef80e09b37cb764e2f04549d0d83
Alex <alexfrancisaaron@gmail.com>
2021-03-01 10:15:42 -0600

    commit tree 7fe26194503cbb25e94183c3b83136ec4ee3db71

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
@@ -52,3 +53,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+class Application < Rails::Application
+  config.web_console.whitelisted_ips = '<IP address here>'
+end 
